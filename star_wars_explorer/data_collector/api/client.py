67d5a2ec3b125ed815670ce7a6d4c11f6459b915
Maciej Baranski <maciej.baranski.gtxm@gmail.com>
2024-06-22 14:25:10 +0200

    Added planet endpoint to star wars client

--- star_wars_explorer/data_collector/api/client.py
+++ star_wars_explorer/data_collector/api/client.py
@@ -7,14 +7,22 @@ class StarWarsAPIClient:
     def __init__(self):
         self.session = Session()
 
-    def get_people(self):
-        # TODO proper error handling
-        response = self.session.get(
-            settings.DATA_COLLECTOR_API_BASE_URL + "api/people/?format=json"
-        )
+    def handle_pagination(self, first_url):
+        response = self.session.get(first_url)
         yield from response.json()["results"]
         next_page = response.json().get("next", None)
         while next_page:
             response = self.session.get(next_page)
             yield from response.json()["results"]
             next_page = response.json().get("next", None)
+
+    def get_people(self):
+        # TODO proper error handling
+        yield from self.handle_pagination(
+            settings.DATA_COLLECTOR_API_BASE_URL + "api/people/?format=json"
+        )
+
+    def get_planets(self):
+        yield from self.handle_pagination(
+            settings.DATA_COLLECTOR_API_BASE_URL + "api/planets/?format=json"
+        )
--- star_wars_explorer/data_collector/tests/api/test_client.py
+++ star_wars_explorer/data_collector/tests/api/test_client.py
@@ -92,3 +92,85 @@ class TestStarWarsAPIClient:
             "Luke Skywalker",
             "Anakin Skywalker",
         ]
+
+    @responses.activate
+    def test_get_planets(self):
+        responses.add(
+            "GET",
+            "https://swapi.dev/api/planets/?format=json",
+            json={
+                "count": 2,
+                "next": "https://swapi.dev/api/planets/?page=2&format=json",
+                "previous": None,
+                "results": [
+                    {
+                        "name": "Tatooine",
+                        "rotation_period": "23",
+                        "orbital_period": "304",
+                        "diameter": "10465",
+                        "climate": "arid",
+                        "gravity": "1 standard",
+                        "terrain": "desert",
+                        "surface_water": "1",
+                        "population": "200000",
+                        "residents": [
+                            "https://swapi.dev/api/people/1/",
+                            "https://swapi.dev/api/people/2/",
+                            "https://swapi.dev/api/people/4/",
+                            "https://swapi.dev/api/people/6/",
+                            "https://swapi.dev/api/people/7/",
+                            "https://swapi.dev/api/people/8/",
+                            "https://swapi.dev/api/people/9/",
+                            "https://swapi.dev/api/people/11/",
+                            "https://swapi.dev/api/people/43/",
+                            "https://swapi.dev/api/people/62/",
+                        ],
+                        "films": [
+                            "https://swapi.dev/api/films/1/",
+                            "https://swapi.dev/api/films/3/",
+                            "https://swapi.dev/api/films/4/",
+                            "https://swapi.dev/api/films/5/",
+                            "https://swapi.dev/api/films/6/",
+                        ],
+                        "created": "2014-12-09T13:50:49.641000Z",
+                        "edited": "2014-12-20T20:58:18.411000Z",
+                        "url": "https://swapi.dev/api/planets/1/",
+                    },
+                ],
+            },
+        )
+        responses.add(
+            "GET",
+            "https://swapi.dev/api/planets/?page=2&format=json",
+            json={
+                "count": 2,
+                "next": None,
+                "previous": "https://swapi.dev/api/planets/?format=json",
+                "results": [
+                    {
+                        "name": "Mirial",
+                        "rotation_period": "unknown",
+                        "orbital_period": "unknown",
+                        "diameter": "unknown",
+                        "climate": "unknown",
+                        "gravity": "unknown",
+                        "terrain": "deserts",
+                        "surface_water": "unknown",
+                        "population": "unknown",
+                        "residents": [
+                            "https://swapi.dev/api/people/64/",
+                            "https://swapi.dev/api/people/65/",
+                        ],
+                        "films": [],
+                        "created": "2014-12-20T16:44:46.318000Z",
+                        "edited": "2014-12-20T20:58:18.508000Z",
+                        "url": "https://swapi.dev/api/planets/51/",
+                    }
+                ],
+            },
+        )
+        client = StarWarsAPIClient()
+        assert [planet["name"] for planet in client.get_planets()] == [
+            "Tatooine",
+            "Mirial",
+        ]
