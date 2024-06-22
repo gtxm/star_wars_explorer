42b29348c7eeb24adf070946125af803b86317d4
Maciej Baranski <maciej.baranski.gtxm@gmail.com>
2024-06-22 14:57:47 +0200

    Added fetch button

--- star_wars_explorer/explorer/views.py
+++ star_wars_explorer/explorer/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from data_collector.models import StarWarsDataCollection
+from data_collector.utils import fetch_latest_dataset
 
 
 def home(request):
     return render(request, "index.html", {"collections": StarWarsDataCollection.objects.all()})
+
+
+def fetch(request):
+    fetch_latest_dataset()
+    return redirect("/")
