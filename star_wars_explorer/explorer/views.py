1950fe869c497f3b5a2c29c1fc50be028be023d7
Maciej Baranski <maciej.baranski.gtxm@gmail.com>
2024-06-22 14:55:04 +0200

    Added list of collections

--- star_wars_explorer/explorer/views.py
+++ star_wars_explorer/explorer/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from data_collector.models import StarWarsDataCollection
+
 
 def home(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {"collections": StarWarsDataCollection.objects.all()})
