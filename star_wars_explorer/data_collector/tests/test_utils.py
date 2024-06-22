5ac1933742924ae778af631ad6b45244a2c09593
Maciej Baranski <maciej.baranski.gtxm@gmail.com>
2024-06-22 14:19:12 +0200

    Added basic transformations for star wars people

--- star_wars_explorer/data_collector/tests/test_utils.py
+++ star_wars_explorer/data_collector/tests/test_utils.py
@@ -3,7 +3,11 @@ from pathlib import Path
 import petl
 import pytest
 
-from ..utils import generate_collection_filename, save_star_wars_data
+from ..utils import (
+    generate_collection_filename,
+    save_star_wars_data,
+    transform_collected_data,
+)
 
 
 def test_generate_collection_filename():
@@ -34,3 +38,52 @@ def test_save_star_wars_data():
 
     # cleanup
     Path(collection.full_path).unlink()
+
+
+def test_transform_collected_data():
+    data = petl.fromdicts(
+        [
+            {
+                "name": "Anakin Skywalker",
+                "height": "188",
+                "mass": "84",
+                "hair_color": "blond",
+                "skin_color": "fair",
+                "eye_color": "blue",
+                "birth_year": "41.9BBY",
+                "gender": "male",
+                "homeworld": "https://swapi.dev/api/planets/1/",
+                "films": [
+                    "https://swapi.dev/api/films/4/",
+                    "https://swapi.dev/api/films/5/",
+                    "https://swapi.dev/api/films/6/",
+                ],
+                "species": [],
+                "vehicles": [
+                    "https://swapi.dev/api/vehicles/44/",
+                    "https://swapi.dev/api/vehicles/46/",
+                ],
+                "starships": [
+                    "https://swapi.dev/api/starships/39/",
+                    "https://swapi.dev/api/starships/59/",
+                    "https://swapi.dev/api/starships/65/",
+                ],
+                "created": "2014-12-10T16:20:44.310000Z",
+                "edited": "2014-12-20T21:17:50.327000Z",
+                "url": "https://swapi.dev/api/people/11/",
+            }
+        ]
+    )
+    transformed_data = transform_collected_data(data)
+    assert transformed_data.header() == (
+        "name",
+        "height",
+        "mass",
+        "hair_color",
+        "skin_color",
+        "eye_color",
+        "birth_year",
+        "gender",
+        "homeworld",
+        "date",
+    )
--- star_wars_explorer/data_collector/utils.py
+++ star_wars_explorer/data_collector/utils.py
@@ -2,6 +2,7 @@ import uuid
 
 import petl
 
+from .api.client import StarWarsAPIClient
 from .models import StarWarsDataCollection
 
 
@@ -15,3 +16,9 @@ def save_star_wars_data(data: petl.Table):
     )
     collection.save_data(data)
     return collection
+
+
+def transform_collected_data(data: petl.Table):
+    return data.addfield("date", lambda rec: rec["edited"][:10]).cutout(
+        "films", "species", "vehicles", "starships", "url", "created", "edited"
+    )
